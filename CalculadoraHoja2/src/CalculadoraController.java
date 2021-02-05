a32e9a27dc86aa97ca0195865dca1dd455788956
moraleseoscar <oestrada.3@gmail.com>
2021-02-05 00:31:48 -0600

    Programa funcionando sin programación defensiva

--- CalculadoraHoja2/src/CalculadoraController.java
+++ CalculadoraHoja2/src/CalculadoraController.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.LinkedList;
 
 /**
@@ -6,6 +8,8 @@ import java.util.LinkedList;
  */
 public class CalculadoraController<E> implements Stack, ICalculadora {
 	private LinkedList<E> lista;
+	private double operandoA;
+	private double operandoB;
 	
 	public CalculadoraController() {
 		lista = new LinkedList<E>();
@@ -18,16 +22,17 @@ public class CalculadoraController<E> implements Stack, ICalculadora {
 	}
 
 	@Override
-	public Object pop() {
+	public String pop() {
 		// TODO Auto-generated method stub
+		String value = peek();
 		lista.removeFirst();
-		return (E) lista.getFirst();
+		return value;
 	}
 
 	@Override
-	public Object peek() {
+	public String peek() {
 		// TODO Auto-generated method stub
-		return (E) lista.getFirst();
+		return (String) lista.getFirst();
 	}
 
 	@Override
@@ -45,13 +50,66 @@ public class CalculadoraController<E> implements Stack, ICalculadora {
 	@Override
 	public Double resolver(String input) {
 		// TODO Auto-generated method stub
-		return null;
+		Double resultado = 0.0;
+		input = input.replaceAll(" ","");
+		for(String ch: input.split("")) {
+			System.out.println(ch);
+			switch(ch){
+			case "+":
+				operandoA = Double.parseDouble(pop());
+				operandoB = Double.parseDouble(pop());
+				resultado = operandoA + operandoB;
+				push(resultado.toString());
+				break;
+			case "-":
+				operandoA = Double.parseDouble(pop());
+				operandoB = Double.parseDouble(pop());
+				resultado = operandoA - operandoB;
+				push(resultado.toString());
+				break;
+			case "*":
+				operandoA = Double.parseDouble(pop());
+				operandoB = Double.parseDouble(pop());
+				resultado = operandoA * operandoB;
+				push(resultado.toString());
+				break;
+			case "/":
+				operandoA = Double.parseDouble(pop());
+				operandoB = Double.parseDouble(pop());
+				resultado = operandoA / operandoB;
+				push(resultado.toString());
+				break;
+			default:
+				push(ch);
+			}
+			System.out.print(lista);
+		}
+		return resultado;
 	}
 
 	@Override
 	public Double resolver(int n1, int n2, char operando) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public String leerDocumento() {
+		// TODO Auto-generated method stub
+		String texto = new String();
+		try {
+			FileReader fr = new FileReader("./docs/datos.txt");
+			BufferedReader entrada = new BufferedReader(fr); 
+			String s;
+			
+			while((s = entrada.readLine()) != null) {
+				texto = s;
+			}
+			
+		}
+		catch(java.io.FileNotFoundException fnfex) {
+			System.out.println("Archivo no encontrado: " + fnfex);}
+		catch(java.io.IOException ioex) {}
+		return texto;
 	}
 	
 	public String toString() {
--- CalculadoraHoja2/src/Main.java
+++ CalculadoraHoja2/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 /**
  * @author Oscar Estrada 20565
@@ -13,30 +12,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scannerObj = new Scanner(System.in);
-        Stack<String> pila = new Stack<String>(); 
+        CalculadoraController c = new CalculadoraController();
+
+        String postfix = c.leerDocumento();
+        System.out.println(c.resolver(postfix));
         
-        System.out.println("Ingrese el codigo del libro: ");
-        String codigo = scannerObj.nextLine();
-        
-        imprimirPila(pila);
-        pila.push("2");
-        pila.push("4");
-        imprimirPila(pila);
-        pila.pop();
-        imprimirPila(pila); 
-		
 	}
-	
-	public static void imprimirPila(Stack<String> pila) {
-        if (pila.empty()) {
-            System.out.print("la pila esta vacia\n\n");
-        } else {
-            System.out.print("la pila contiene: ");
-            for (String numero : pila) {
-                System.out.printf("%s ", numero);
-            }
-            System.out.print("(superior) \n\n");
-        }
-    }
 
 }
